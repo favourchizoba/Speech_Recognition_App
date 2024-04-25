d209c92de8cbe958c67c18f4ed088fd41f9a4340
chibuezechizoba <156071798+favourchizoba@users.noreply.github.com>
2024-04-25 14:14:51 +0100

    Add files via upload

---
+++ speechReg.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import speech_recognition as sr
+import pyttsx3
+import time
+import pandas as pd
+
+# Create a function for Speech Trancription
+def transcribe_speech():
+    # Initialize recognizer class
+    r = sr.Recognizer()
+
+    # Reading Microphone as source
+    with sr.Microphone() as source:
+
+        # create a streamlit spinner that shows progress
+        with st.spinner(text='Silence pls, Caliberating background noise.....'):
+            time.sleep(2)
+
+        r.adjust_for_ambient_noise(source, duration = 1) # ..... Adjust the sorround noise
+        st.info("Speak now...")
+
+        audio_text = r.listen(source) #........................ listen for speech and store in audio_text variable
+        with st.spinner(text='Transcribing your voice to text'):
+            time.sleep(2)
+
+        try:
+            # using Google speech recognition to recognise the audio
+            text = r.recognize_google(audio_text)
+            # print(f' Did you say {text} ?')
+            return text
+        except:
+            return "Sorry, I did not get that."
+
+# # Create a function for text Talkback
+# def Text_Speaker(your_command):
+#     speaker_engine = pyttsx3.init() #................ initiate the talkback engine
+#     speaker_engine.say(your_command) #............... Speak the command
+#     speaker_engine.runAndWait() #.................... Run the engine
+
+
+
+def main():
+    st.title("Speech Recognition App")
+    st.markdown("<h6 style = 'top_margin: 0rem; color: #F2921D'>Built by Frances</h6>", unsafe_allow_html = True)
+    st.header('Project Background Information', divider = True)
+    st.write('This technology utilizes algorithms and machine learning models to analyze audio input, identify spoken words, and transcribe them into written text.') 
+    st.write("Click on the microphone to start speaking:")
+
+    # add a button to trigger speech recognition
+    if st.button("Start Recording"):
+        your_words_in_text = transcribe_speech()
+        st.write("Transcription: ", your_words_in_text)
+if __name__ == "__main__":
+    main()
